bc3ec35a6e2e95584ad40cc948ccd7243bbc1156
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2023-06-09 13:19:02 +0200

    initialized digitspan repository

---
+++ instructions.js
@@ -0,0 +1,37 @@
+/*
+ * In this file the instructions are defined that are used throughout
+ * the experiment.
+ * Make sure when editing this file, the file is stored with
+ * utf8 text encoding.
+ */
+
+const PRE_PRACTICE_INSTRUCTION =
+    "<strong>Beste deelnemer,</strong><br><br>"         +
+    "Dank je voor het deelnemen in dit experiment."     +
+    "<br><br>"                                          +
+    "Dit is een geheugen test. Ik presenteer een lijst "+
+    "van cijfer via het geluidssysteem"                 +
+    "<br><br>"                                          +
+    "Jouw taak is de cijfers in te typen in dezelfde "  +
+    "volgorde en dan op OK te klikken."                 +
+    "<br><br>"                                          +
+    "Eerst doen we een oefening."                       +
+    "<br><br>"                                          +
+    "<i>Klik op \"Ga verder\" wanneer je klaar bent om "+
+    "te starten</i>"
+    ;
+
+const PRE_TEST_INSTRUCTION =
+    "Einde van de oefening.<br><br>"                 +
+    "<i>Klik op \"Ga verder\" om de taak te beginnen</i>"
+    ;
+
+const DEBRIEF_MESSAGE = `
+    <h1>Einde van het experiment</h1>
+    <br>
+    <br>
+    <h2>Dank je wel!</h2>
+    `;
+
+const RECALL_PROMPT = "<h1>Herhaal in gegeven volgorde</h1>";
+
